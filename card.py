5b53bfd3bc00d6e2d9c1fa2eab0cb06f0aae6c25
Sam Odio <sam@odio.com>
2011-08-07 15:57:29 -0700

    Alert if resource goes offline

--- card.py
+++ card.py
@@ -9,11 +9,16 @@ threshold = 50.0 #alert threshold
 n = 0
 
 while n < (duration*60*60/interval):
-    result = json.loads(urllib2.urlopen('http://jonathanstark.com/card/api/latest').read())
+    try:
+        result = json.loads(urllib2.urlopen('http://jonathanstark.com/card/api/latest').read())
+    except:
+        print "Error loading resource."
+        commands.getstatusoutput('open ./play.app') #Play your iTunes library
     balance = float(result['balance']['amount'])
     n += 1
     if balance > threshold:
         commands.getstatusoutput('open ./play.app') #Play your iTunes library
+        print "BALANCE IS %s" % balance
     else:
         print "%s is not over %s" % (balance, threshold)
     time.sleep(interval)
